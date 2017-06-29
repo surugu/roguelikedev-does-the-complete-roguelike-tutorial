f7cd1601cbd3551e68c8ff8dd438bf65c95dd4cb
Johnny <johnny.winner@gmail.com>
2017-06-29 04:07:13 -0500

    Draw the game map

--- engine.py
+++ engine.py
@@ -4,12 +4,20 @@ except ImportError:
     import tcod as libtcod
 
 from entity import Entity
+from map_objects.game_map import GameMap
 from input_handlers import handle_keys
 from render_functions import render_all, clear_all
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
 
     player = Entity(screen_width // 2, screen_height // 2, '@', libtcod.white)
     npc = Entity((screen_width // 2) - 5, (screen_height // 2) - 5, '@', libtcod.yellow)
@@ -24,13 +32,15 @@ def main():
 
     con = libtcod.console_new(screen_width, screen_height)
 
+    game_map = GameMap(map_width, map_height)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
 
         clear_all(con, entities)
